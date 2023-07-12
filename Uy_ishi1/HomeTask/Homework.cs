6f7f780c2285b15a6811e0929fc4cd1822bd4eba
MuhammadrizoVositov <vositovmuhammadrizo13@gmail.com>
2023-07-12 16:03:47 +0500

    add 1 - homework

---
+++ Uy_ishi1/HomeTask/Homework.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uy_ishi1.HomeTask
+{
+    internal class Homework
+    {
+        public static void Main(string[] args)
+        {
+            //         1 qism
+            //string name = Console.ReadLine();
+            //Console.WriteLine("What is your name : " + name);
+            //string surname = Console.ReadLine();
+            //Console.WriteLine("What is your surname:");
+            //int age = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("How old are you");
+
+            //        2 qism 
+
+
+            //DateTime date = DateTime.Now;
+            string g = "1970 - yil 1-yanvar";
+            Console.WriteLine(g);
+            int f = 75;
+            Console.WriteLine(f + " yosh");
+            //double m = 35.5;
+            //Console.WriteLine(m + "$");
+            //        3 qism 
+
+            //int age = 24;
+            // Console.WriteLine(age);
+
+        }
+
+
+    }
+
+
+
+
+}
+
+
---
+++ Uy_ishi1/Program.cs
@@ -0,0 +1,26 @@
+﻿//primitive type
+//bool hasMoney = true;
+//char Dollar = '$';
+//byte age = 50;
+//sbyte Roomteperature = 22;
+//short balance = -1000;
+//ushort distance = 60000;
+//int Popuation = 2000000;
+//uint manfacture = 40000000;
+//long planet = 9000000000000;
+//ulong planetB = 2000000000;
+//float weiht = 22.838334f;
+//double pi = 3.6985686568965;
+//decimal piDecimal = 4.5968658655966m;
+
+//Difinition
+
+
+
+
+
+
+//Literals
+
+
+
